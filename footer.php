c85fa6cd48140c00c378fed94b9bf3a21a60c492
hendronf <mail@fearghal.co.uk>
2013-07-28 16:27:37 +0200

    Turned Blog into Changelog and Added associated Styles
    ToDo: Style the Changelog overview page (blog)

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-			<a href="https://github.com/hendronf/Stile" target="_blank">Stile</a> by <a href="http://fearghal.co.uk/" target="_blank">Fearghal</a> - <a href="<?php echo wp_logout_url(); ?>" title="Logout" class="logouturl">Logout</a>
+			<a href="https://github.com/hendronf/Stile" target="_blank">Stile</a> by <a href="http://fearghal.co.uk/" target="_blank">Fearghal</a> - <a href="/changelog">Change Log</a> - <a href="<?php echo wp_logout_url(); ?>" title="Logout" class="logouturl">Logout</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
