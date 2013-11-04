5f5d19bd141eca4f2709597caf68353ca4250140
hendronf <mail@fearghal.co.uk>
2013-11-04 12:52:34 +0800

    Removed Open Sans from main stylesheet
    This is an IN Only font, and is now in the admin backend of the
    specific post, so It is called only on the required page.

--- header.php
+++ header.php
@@ -33,7 +33,6 @@ if ($user_ID == '') {
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700' rel='stylesheet' type='text/css'> <!-- Used in the typography section of the site. -->
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
