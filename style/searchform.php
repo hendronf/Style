d7a1df6e59d5aced03025876f7a8567b4f4b742e
Fearghal Hendron <mail@fearghal.co.uk>
2014-07-28 16:52:22 +0800

    Minor Updates to formatting
    Fixed a few typos
    Repositioned a few things in the file

--- style/searchform.php
+++ style/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
-        <input type="text" value="" class="form-control" name="s" id="s" placeholder="Search" />
+        <input type="search" value="" class="form-control" name="s" id="s" placeholder="Search" />
         <input type="submit" id="searchsubmit" value="Search" />
     </div>
 </form>
